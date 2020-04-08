2621f61b09da97e0a79e6cfd995631557f53019b
Oleg <krasno1986@gmail.com>
2020-04-08 12:45:07 +0300

    add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  devise :database_authenticatable, :registerable, :confirmable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
 
   validates :username, presence: true, uniqueness: true
 
   has_many :companies, dependent: :destroy
+
+  def jwt_payload
+    { foo: 'bar' }
+  end
 end
